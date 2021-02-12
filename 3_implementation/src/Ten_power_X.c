1f5c3f12a3056cd05bd6d1e0ddbf4f29a324fc12
GaneshReddy99 <78849231+GaneshReddy99@users.noreply.github.com>
2021-02-12 17:31:35 +0530

    Create Ten_power_X.c

---
+++ 3_implementation/src/Ten_power_X.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+#include "Ten_power_X"
+int Ten_power_X()
+{
+   
+    double num, result;
+    printf("10^x-->Enter x value");
+    scanf("%lf",&num);
+    result = pow(10, num);
+    printf("10^%d = %.2f",num, result);
+
+   
+}
