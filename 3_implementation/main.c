3d3b498039eea00a98ae8df78bbd70e87049f082
vishalroshanJ <78852893+vishalroshanJ@users.noreply.github.com>
2021-02-12 17:05:21 +0530

    Create main.c
     first function in your program that is executed when it begins executing

---
+++ 3_implementation/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "addition.h"
+#include "subtraction.h"
+#include "multiplication.h"
+#include "division.h"
+#include "percentage.h"
+#include "modulus.h"
+#include "factorial.h"
+#include "exponential.h"
+#include "prime.h"
+
+void addition();
+void division();
+void exponential();
+void factorial();
+void modulus();
+void multiplication();
+void percentage();
+void subtraction();
+void prime();
+
+void calc_inst()
+{
+	
+	printf("******* Press 'Q' or 'q' to quit ");
+	printf("the program ********\n");
+	printf("***** Press 'H' or 'h' to display ");
+	printf("below options *****\n\n");
+	printf("Enter 'C' or 'c' to clear the screen and");
+	printf(" display available option \n\n");
+}
+
+void calc_opr()
+{
+	printf("\n             Welcome to C calculator \n\n");
+	calc_inst();
+
+	printf("Enter A or a for Addition \n");
+	printf("Enter S or s for Subtraction \n");
+	printf("Enter M or m for Multiplication \n");
+	printf("Enter D or d for Division \n");
+	printf("Enter P or p for Percentage \n");
+	printf("Enter L or l for Modulus \n");
+	printf("Enter F or f for Factorial \n");
+	printf("Enter E or e for Exponential \n");
+	printf("Enter P or p for Checking prime \n");
+}
+
+int main(void)
+{
+	int X = 1;
+	char Calc_opr_input;
+	char respo;
+
+	calc_opr();
+	scanf("%c", &Calc_opr_input);
+	switch(Calc_opr_input)
+	{
+		case 'A': 
+		case 'a': addition();
+				break;
+		case 'S': 
+		case 's': subtraction();
+				break;
+		case 'M': 
+		case 'm': multiplication();
+				break;
+		case 'D': 
+		case 'd': division();
+				break;
+		case '%': percentage();
+				break;
+		case 'L':
+		case 'l': modulus();
+				break;
+		case 'F': 
+		case 'f': factorial();
+				break;
+		case 'E': 
+		case 'e': exponential();
+				break;
+		case 'P': 
+		case 'p': prime();
+				break;					
+		case 'Q': 
+		case 'q': exit(0);
+				break;
+		case 'H': case 'h': calc_opr();
+							break;
+		default : 
+			printf("\n**********You have entered unavailable option");
+    		printf("***********\n");
+    		printf("\n*****Please Enter any one of below available ");
+    		printf("options****\n");
+			calc_opr();
+
+	}
+
+}
