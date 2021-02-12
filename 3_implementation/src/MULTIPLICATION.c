99ebe3e018ed39ecee5e7b032661b891ef5f6ee8
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-12 17:23:06 +0530

    Create MULTIPLICATION.c
    it contains multiplication of two numbers.

---
+++ 3_implementation/src/MULTIPLICATION.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "multiplication.h"
+
+void multiplication()
+{
+  int i;
+  float j=1;
+  printf("enter the number of elements to mul: ");
+  int n;
+  scanf("%d",&n);
+  printf("enter the numbers one by one: \n",n);
+  float a[n];
+  for(i=0;i<n;i++)
+  {
+     scanf("%f",&a[i]);
+      j=j*a[i];
+  }
+  printf("product of numbers is: %.1f",j);
+}
