c16bb5e32a9424de5a29d070036223141538d5f4
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-02-12 17:08:21 +0530

    Create TAN.c
    Tan function

---
+++ 3_implementation/src/TAN.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+ 
+#include <math.h>
+#include "tan.h"
+
+void tan()
+ 
+int main()
+ 
+{
+       
+       float n;
+       scanf("%f",&n);
+       float result=(3.141592*n)/180;
+       float tan_value = tan(result);
+      
+       printf("%f",tan_value);
+       
+       return 0;
+}
