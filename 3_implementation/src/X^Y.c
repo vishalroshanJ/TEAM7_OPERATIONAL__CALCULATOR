ff77c922fad6d97419f51b0dac732a8a3d8a2376
GaneshReddy99 <78849231+GaneshReddy99@users.noreply.github.com>
2021-02-12 17:29:57 +0530

    Create X^Y.c

---
+++ 3_implementation/src/X^Y.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+#include "X^Y"
+void X^Y()
+{
+   
+    double num1,num2, result;
+    printf("Enter Base value");
+    scanf("%lf",&num1);
+    printf("Enter power value");
+    scanf("%lf",&num2);
+    result = pow(num1, num2);
+    printf("%lf^%lf = %.2f",num1,num2, result);
+
+   
+}
