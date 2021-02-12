b344d152ff8b36c119fcdaa12ba22c129924602a
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-12 17:29:02 +0530

    Create DIVISION.c
    contains division of numbers.

---
+++ 3_implementation/src/DIVISION.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "division.h"
+{
+  int i;
+  float j=1;
+  printf("enter the number of elements to div: ");
+  int n;
+  scanf("%d",&n);
+  printf("enter the elements one by one: \n",n);
+ float a[n];
+  for(i=0;i<n;i++)
+  {
+     scanf("%f",&a[i]);
+     if(i==0)
+     {
+         j=a[i];
+     }
+     else
+      j=j/a[i];
+  }
+  printf("quotient of the elements is: %f",j);
+}
