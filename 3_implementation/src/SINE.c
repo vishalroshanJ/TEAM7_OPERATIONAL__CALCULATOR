268413a6bfa40f5682894906814ec5056fe847d4
vishalroshanJ <78852893+vishalroshanJ@users.noreply.github.com>
2021-02-12 17:06:30 +0530

    Update SINE.c

--- 3_implementation/src/SINE.c
+++ 3_implementation/src/SINE.c
@@ -3,19 +3,12 @@
 #include "sine.h"
 
 void sine()
-int main()
 {
   double sinValue, n;
-  
   printf("Enter the Value to calculate Sine :  ");
-  
   scanf("%lf", &n);
-  
   sinValue = sin(n);
-  
   printf("\n The Sine value of %lf = %f ", n, sinValue);
-  
-  return 0;
 }
 
 
