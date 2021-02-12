d93c6d7422bb5e9b2c0432a92b6ad36d969f87a1
GaneshReddy99 <78849231+GaneshReddy99@users.noreply.github.com>
2021-02-12 17:23:20 +0530

    Create X^2.c

---
+++ 3_implementation/src/X^2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+#include "X^2"
+void X^2()
+{
+   
+    double num, result;
+    printf("x^2-->Enter x value");
+    scanf("%lf",&num);
+    result = pow(num, 2);
+    printf("x^2 = %.2f", result);
+
+    
+}
