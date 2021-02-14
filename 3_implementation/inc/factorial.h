6672364f03e5ae81b82abb02f1d74b99db30ea8c
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-02-14 17:36:27 +0530

    Update factorial.h

--- 3_implementation/inc/factorial.h
+++ 3_implementation/inc/factorial.h
@@ -1,4 +1,24 @@
-#ifndef factorial_h
-#define factorial_h
-void factorial();
-#endif 
+#include <stdio.h>
+
+void factorial()
+{
+    int i,n,result=1;
+printf("Enter the number:");
+scanf("%d",&n);
+    if (n>=1)
+    {
+        for(i=1;i<=n;i++)
+        {
+    result=result*i;
+        }
+       printf("factorial of %d = %d \n",n,result);
+        }
+    else if(n<0)
+    {
+        printf("factorial of %d = Not Valid \n",n);
+        }
+        else
+        {
+         printf("factorial of %d = 1 \n",n);
+        }
+}
