b9543e61dd066f1f5516dbde4983324b5cb51da4
Aditya Choudhary <78849471+adityaltts@users.noreply.github.com>
2021-02-14 20:00:08 +0530

    Update tan.c

--- 3_implementation/src/tan.c
+++ 3_implementation/src/tan.c
@@ -1,16 +1,17 @@
-
 #include <stdio.h>
 #include <math.h>
-#include "tan.h"
-
-void tan()
+int main()
 {
-       
        float n;
-       scanf("%f",&n);
+       printf("Enter the Value in Degrees:");
+       while (scanf("%f",&n)==0)
+{
+printf("Invalid input. Please Vaild Input: ");
+scanf("%*s");
+}
        float result=(3.141592*n)/180;
        float tan_value = tan(result);
       
-       printf("%f",tan_value);
+       printf("%.2f",tan_value);
        
 }
