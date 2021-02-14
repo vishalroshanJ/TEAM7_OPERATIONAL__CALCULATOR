b904d45d14d3d18679d9a802979f8efc2b51824c
GaneshReddy99 <78849231+GaneshReddy99@users.noreply.github.com>
2021-02-14 19:50:25 +0530

    Update ceil.h

--- 3_implementation/inc/ceil.h
+++ 3_implementation/inc/ceil.h
@@ -1,10 +1,14 @@
-#include <stdio.h>
-
+##include <stdio.h>
+#include "ceilval.h"
 void ceilval()
 {
 float n,result;
 printf("Enter the number:");
-scanf("%f",&n);
+while (scanf("%f",&n)==0)
+{
+printf("Invalid input. Please Vaild Input: ");
+scanf("%*s");
+}
 result=ceil(n);
-printf("Ceil of %f = %f \n",n,result);
+printf("Ceil of %.2f = %.2f \n",n,result);
 }
