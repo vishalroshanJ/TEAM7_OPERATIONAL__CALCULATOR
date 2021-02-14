130d472a2585bc833159a983a362fdbcd0f4d923
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-14 17:34:53 +0530

    Update DIVISION.h

--- 3_implementation/inc/DIVISION.h
+++ 3_implementation/inc/DIVISION.h
@@ -1,6 +1,23 @@
-#ifndef division_h
-#define division_h
+#include <stdio.h>
 
-void division();
-
-#endif
+void division()
+{
+  int i;
+  float j=1;
+  printf("enter the number of elements to div: ");
+  int n;
+  scanf("%d",&n);
+  printf("please enter the elements one by one: \n",n);
+ float a[n];
+  for(i=0;i<n;i++)
+  {
+     scanf("%f",&a[i]);
+     if(i==0)
+     {
+         j=a[i];
+     }
+     else
+      j=j/a[i];
+  }
+  printf("quotient of the elements is: %f",j);
+}
