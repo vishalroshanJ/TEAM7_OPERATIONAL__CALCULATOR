68d1cdb02bb511b5eb8390e9ea87b0deead9a879
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-14 17:32:52 +0530

    Update addition.h

--- 3_implementation/inc/addition.h
+++ 3_implementation/inc/addition.h
@@ -1,6 +1,15 @@
-#ifndef addition_h
-#define addition_h
-
-void addition();
-
-#endif
+#include <stdio.h>
+void addition()
+{
+    int n, total=0, k=0, number;
+    printf("\nEnter the number of elements you want to add:");
+    scanf("%d",&n);
+    printf("Please enter %d numbers one by one: \n",n);
+    while(k<n)
+    { 
+        scanf("%d",&number);
+        total=total+number;
+        k=k+1;
+    }
+    printf("Sum of %d numbers = %d \n",n,total);
+}
