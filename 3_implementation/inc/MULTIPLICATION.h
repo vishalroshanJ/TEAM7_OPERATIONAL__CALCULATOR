3a24cd6129db7bd55dd4a7d24ed936d02efa70d4
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-14 17:36:55 +0530

    Update MULTIPLICATION.h

--- 3_implementation/inc/MULTIPLICATION.h
+++ 3_implementation/inc/MULTIPLICATION.h
@@ -1,6 +1,17 @@
-#ifndef multiplication_h
-#define multiplication_h
-
-void multiplication();
-
-#endif
+#include <stdio.h>
+void multiplication()
+{
+  int i;
+  float j=1;
+  printf("enter the number of elements to mul: ");
+  int n;
+  scanf("%d",&n);
+  printf("please enter the numbers one by one: \n",n);
+  float a[n];
+  for(i=0;i<n;i++)
+  {
+     scanf("%f",&a[i]);
+      j=j*a[i];
+  }
+  printf("product of numbers is: %.1f",j);
+}
