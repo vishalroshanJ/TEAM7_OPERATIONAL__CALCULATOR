89663cd30292e1c8d0febc2c01614e1da0b1ef04
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-02-14 17:37:14 +0530

    Update floor.h

--- 3_implementation/inc/floor.h
+++ 3_implementation/inc/floor.h
@@ -1,4 +1,10 @@
-#ifndef floorval_h
-#define floorval_h
-void floorval();
-#endif
+#include <stdio.h>
+
+void floorval()
+{
+float n,result;
+printf("Enter the number:");
+scanf("%f",&n);
+result=floor(n);
+printf("floor of %f = %f \n",n,result);
+}
