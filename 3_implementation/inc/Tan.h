14887be4b02d3574809b2d6bcf853390da0b99a6
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-02-14 17:33:39 +0530

    Update Tan.h

--- 3_implementation/inc/Tan.h
+++ 3_implementation/inc/Tan.h
@@ -1,6 +1,15 @@
-#ifndef tan_h
-#define tan_h
+#include <stdio.h>
+#include <math.h>
+#include "tan.h"
 
-void tan();
-
-#endif
+void tan()
+{
+       
+       float n;
+       scanf("%f",&n);
+       float result=(3.141592*n)/180;
+       float tan_value = tan(result);
+      
+       printf("%f",tan_value);
+       
+}
