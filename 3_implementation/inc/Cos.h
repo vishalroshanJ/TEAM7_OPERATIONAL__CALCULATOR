e7924a0c7bd3f5f703c7057712fa953789f7f030
Aditya Choudhary <78849471+adityaltts@users.noreply.github.com>
2021-02-14 20:28:19 +0530

    Update Cos.h

--- 3_implementation/inc/Cos.h
+++ 3_implementation/inc/Cos.h
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <math.h>
-#define PI 3.141592654
-
-void cosin()
+#include "cos.h"
+void cos()
 {
-   double arg = 30, result;
-
-   // Converting to radian
-   arg = (arg * PI) / 180;
-   result = cos(arg);
-
-   printf("cos of %.2lf radian = %.2lf", arg, result);
-
-   return 0;
+       float n;
+       printf("Enter the Value in Degrees:");
+       while (scanf("%f",&n)==0)
+{
+printf("Invalid input. Please Vaild Input: ");
+scanf("%*s");
+}
+       float result=(3.141592*n)/180;
+       float cos_value = cos(result);
+      
+       printf("%.2f",cos_value);
+       
 }
