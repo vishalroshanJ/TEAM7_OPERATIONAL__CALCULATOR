016e04d45c8497c59fa4eafb34cc45ffc0fbecec
Aditya Choudhary <78849471+adityaltts@users.noreply.github.com>
2021-02-14 20:32:17 +0530

    Update xy.h

--- 3_implementation/inc/xy.h
+++ 3_implementation/inc/xy.h
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include<math.h>
-
+#include "xy.h"
 void xy()
 {
-   
-    double num1,num2, result;
-    printf("Enter Base value");
-    scanf("%lf",&num1);
-    printf("Enter power value");
-    scanf("%lf",&num2);
+   double num1,num2, result;
+    printf("Enter Base value:");
+    while (scanf("%lf",&num1)==0)
+{
+printf("Invalid input. Please Vaild Input: ");
+scanf("%*s");
+} 
+    printf("Enter power value:");
+    while (scanf("%lf",&num2)==0)
+{
+printf("Invalid input. Please Vaild Input: ");
+scanf("%*s");
+} 
     result = pow(num1, num2);
-    printf("%lf^%lf = %.2f",num1,num2, result);
-
-   
+    printf("%.2lf^%.2lf = %.2f",num1,num2, result);
 }
