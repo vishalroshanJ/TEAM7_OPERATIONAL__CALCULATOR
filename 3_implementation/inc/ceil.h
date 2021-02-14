30ada09e38c93885e6644718bff66fbc73fceda3
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-14 17:33:53 +0530

    Update ceil.h

--- 3_implementation/inc/ceil.h
+++ 3_implementation/inc/ceil.h
@@ -1,4 +1,10 @@
-#ifndef ceilval_h
-#define ceilval_h
-void ceilval();
-#endif
+#include <stdio.h>
+
+void ceilval()
+{
+float n,result;
+printf("Enter the number:");
+scanf("%f",&n);
+result=ceil(n);
+printf("Ceil of %f = %f \n",n,result);
+}
