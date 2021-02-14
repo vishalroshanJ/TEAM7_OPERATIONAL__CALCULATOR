24c4f617b5679ab31ff12b380577ae6a80cb689c
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-02-14 17:38:43 +0530

    Update xy.h

--- 3_implementation/inc/xy.h
+++ 3_implementation/inc/xy.h
@@ -1,6 +1,16 @@
-#ifndef xy_h
-#define xy_h
+#include <stdio.h>
+#include<math.h>
 
-void xy();
+void xy()
+{
+   
+    double num1,num2, result;
+    printf("Enter Base value");
+    scanf("%lf",&num1);
+    printf("Enter power value");
+    scanf("%lf",&num2);
+    result = pow(num1, num2);
+    printf("%lf^%lf = %.2f",num1,num2, result);
 
-#endif
+   
+}
