6647abcd469ad3e0c0a8bb5027f53de158ae51d4
vishalroshanJ <78852893+vishalroshanJ@users.noreply.github.com>
2021-02-15 09:40:51 +0530

    Update test_addition.h

--- 3_implementation/test/test_addition.h
+++ 3_implementation/test/test_addition.h
@@ -1,6 +1,22 @@
-#ifndef __test_addition_H__
-#define __test_addition_H__
+#include "unity.h"
+#include "addition.h"
+#include "test_addition.h"
 
-int test_addition(void);
+void test_addition_basic(void)
+{
+    // H_01
+    int h01_arr[] = {12, 24};
+    int h01_n = sizeof(h01_arr)/sizeof(int);
+	TEST_ASSERT_EQUAL( 36, addition(h01_n, (int*)&h01_arr));
+}
 
-#endif
+int test_addition(void)
+{
+	/* Initiate the Unity Test Framework */
+	UNITY_BEGIN();
+
+	/* Run Test functions */
+	RUN_TEST(test_addition_basic);
+
+	/* Close the Unity Test Framework */
+	return UNITY_END();
